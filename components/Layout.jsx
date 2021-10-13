3283389d44c22faef777bc90ad8e276e84db9dea
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-10-13 01:50:20 +0000

    Restarted due to a server error

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,12 @@
 import styles from '../styles/Layout.module.css'
 import Nav from './Nav'
-import Header from './Header'
-import ArticleList from './ArticleList'
+
 const Layout = ({children}) => {
   return (
   <>
     <Nav />
     <div className={styles.container}>
       <main className={styles.main}>
-        <Header />
-        <ArticleList />
         {children}
       </main>
     </div>
