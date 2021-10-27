ea114a7d801884ab5fdc9a9c4ac6c70d33245c31
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-10-27 04:18:51 +0000

    mongodb boiler plate

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,3 +1,12 @@
+// const { connectToDatabase } = require('../../utils/database');
+// const ObjectId = require('mongodb').ObjectId;
+
+// export default async function handler(req, res) {
+//   let { db } = await connectToDatabase();
+//   res.json()
+
+
+// }
 import nextConnect from "next-connect";
 import middleware from "../../utils/database";
 
--- utils/database.js
+++ utils/database.js
@@ -1,19 +1,48 @@
 import { MongoClient } from 'mongodb';
-import nextConnect from 'next-connect';
 
-const {DB_URI, DB_NAME, DB_PORT} = process.env;
+const MONGODB_URI = process.env.DB_URI;
+const MONGODB_DB = process.env.DB_NAME;
 
-
-const client = new MongoClient(DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-async function database(req, res, next) {
-  await client.connect();
-  req.dbClient = client;
-  req.db = client.db('CalenderApp');
-  return next();
+// check the MongoDB URI
+if (!MONGODB_URI) {
+  throw new Error('Define the MONGODB_URI environmental variable');
 }
-const middleware = nextConnect();
-middleware.use(database);
-export default middleware;
+
+// check the MongoDB DB
+if (!MONGODB_DB) {
+  throw new Error('Define the MONGODB_DB environmental variable');
+}
+
+let cachedClient = null;
+let cachedDb = null;
+
+export async function connectToDatabase() {
+  // check the cached.
+  if (cachedClient && cachedDb) {
+    // load from cache
+    return {
+      client: cachedClient,
+      db: cachedDb,
+    };
+  }
+
+  // set the connection options
+  const opts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  // Connect to cluster
+  let client = new MongoClient(MONGODB_URI, opts);
+  await client.connect();
+  let db = client.db(MONGODB_DB);
+
+  // set cache
+  cachedClient = client;
+  cachedDb = db;
+
+  return {
+    client: cachedClient,
+    db: cachedDb,
+  };
+}
